6919d5632576e408a7eb1c40c45fdb994a229c94
lining-lo <2092576148@qq.com>
2026-09-11 15:55:29 +0800

    2026-09-11 代码更新

---
+++ java-learn/day04/src/com/demo/permission/PermissionDemo.java
@@ -0,0 +1,19 @@
+package com.demo.permission;
+
+import com.demo.permission.demo.A;
+
+public class PermissionDemo {
+    /*
+        权限修饰符:
+
+            private         同一个类中
+            (default)       同一个类中       同一个包中
+            protected       同一个类中       同一个包中       不同包的子类
+            public          任意位置
+     */
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.num);
+        a.method();
+    }
+}
---
+++ java-learn/day04/src/com/demo/permission/demo/A.java
@@ -0,0 +1,10 @@
+package com.demo.permission.demo;
+
+public class A {
+
+    public int num;
+
+    public void method() {
+        System.out.println("method...");
+    }
+}
