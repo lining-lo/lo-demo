4c69ce1fc422a0cf9152fc50f1a682269faa036a
lining-lo <2092576148@qq.com>
2026-09-11 16:07:17 +0800

    2026-09-11 代码更新

---
+++ java-learn/day04/src/com/demo/test/ArrayToolsTest.java
@@ -0,0 +1,16 @@
+package com.demo.test;
+
+import com.demo.tools.ArrayTools;
+
+public class ArrayToolsTest {
+    public static void main(String[] args) {
+        int[] arr = {11, 22, 33, 44, 55};
+
+        int max = ArrayTools.getMax(arr);
+        int min = ArrayTools.getMin(arr);
+        System.out.println("最大值为:" + max);
+        System.out.println("最小值为:" + min);
+        ArrayTools.printArray(arr);
+
+    }
+}
