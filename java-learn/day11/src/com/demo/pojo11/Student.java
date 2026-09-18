d886443de38446ab349de0b99fad339cdc127e72
lining-lo <2092576148@qq.com>
2026-09-18 22:25:20 +0800

    2026-09-18 代码更新

---
+++ java-learn/day11/src/com/demo/pojo11/Student.java
@@ -0,0 +1,13 @@
+package com.demo.pojo11;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String name;
+    private int age;
+}
