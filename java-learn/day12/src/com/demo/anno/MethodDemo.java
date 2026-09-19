075abdc1e087cb60235365b782181bd82830261b
lining-lo <2092576148@qq.com>
2026-09-19 09:48:42 +0800

    2026-09-19 代码更新

---
+++ java-learn/day12/src/com/demo/anno/MethodDemo.java
@@ -0,0 +1,20 @@
+package com.demo.anno;
+
+public class MethodDemo {
+
+    @Test
+    public void print() {
+        System.out.println("print");
+    }
+
+
+    public void show() {
+        System.out.println("show");
+    }
+
+    @Test
+    public void method() {
+        System.out.println("method");
+    }
+
+}
---
+++ java-learn/day12/src/com/demo/anno/MyAnno2.java
@@ -0,0 +1,5 @@
+package com.demo.anno;
+
+public @interface MyAnno2 {
+    String value();
+}
